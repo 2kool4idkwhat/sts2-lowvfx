888abed0e8a7f69f92ecb60592bcd83eccbbda97
Luna 2kool4idkwhat <120322313+2kool4idkwhat@users.noreply.github.com>
2026-04-29 15:05:48 +0200

    disable the sandfalls in the insatiable's background

---
+++ src/CombatVFX/TheInsatiable.cs
@@ -0,0 +1,34 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using Godot;
+
+namespace LowVFX;
+
+// disable the sandfalls in the insatiable's background
+[HarmonyPatch(typeof(NCombatBackground), nameof(NCombatBackground.Create))]
+public static class NCombatBackground_Sandfalls_Patch
+{
+
+    public static void Postfix(NCombatBackground __result)
+    {
+        if (ModConfig.KeepInsatiableBgSandfalls) return;
+
+        if (!__result.SceneFilePath.Contains("the_insatiable_boss")) return;
+
+        MainFile.Logger.Info("Disabling sandfalls");
+
+        for (var i = 1; i <= 9; i++)
+        {
+            var sandfall = __result.GetNodeOrNull<Node2D>($"gpu waterfall {i}");
+            if (sandfall == null)
+            {
+                MainFile.Logger.Info($"Skipping sandfall {i}");
+                continue;
+            }
+
+            sandfall.Visible = false;
+            sandfall.ProcessMode = Node.ProcessModeEnum.Disabled;
+        }
+    }
+
+}
--- src/ModConfig.cs
+++ src/ModConfig.cs
@@ -5,4 +5,7 @@ namespace LowVFX;
 internal class ModConfig : SimpleModConfig
 {
     public static bool KeepCardTransformVfx { get; set; } = false;
+
+    [ConfigHoverTip]
+    public static bool KeepInsatiableBgSandfalls { get; set; } = false;
 }
