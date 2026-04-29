1b56e36600e8d7c44ddfb839b7a61e54668c2dda
Luna 2kool4idkwhat <120322313+2kool4idkwhat@users.noreply.github.com>
2026-04-29 12:16:45 +0200

    disable the glow around uncommon/rare cards in card rewards

---
+++ src/NonCombatVFX/CardRewards.cs
@@ -0,0 +1,18 @@
+// disable the glow around uncommon/rare cards in card rewards (and a few other
+// places like card potions)
+
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+
+namespace LowVFX;
+
+[HarmonyPatch(typeof(NCard), nameof(NCard.ActivateRewardScreenGlow))]
+public static class NCard_Patch
+{
+
+    public static bool Prefix()
+    {
+        return false;
+    }
+
+}
